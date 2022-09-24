3c36195ab7a5ef188ee54bec586977e7ead9ccf6
iaroslavski <43264149+iaroslavski@users.noreply.github.com>
2022-09-25 01:56:01 +0300

    Update BentleyBasher.java

--- test/BentleyBasher.java
+++ test/BentleyBasher.java
@@ -128,6 +128,7 @@ public class BentleyBasher {
         }
     }
 
+    
     private static double[] times = new double[2];
     private static int[] golden = new int[MAX_N];
     private static int[] result = new int[MAX_N];
