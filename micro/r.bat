d8e2bebdc8842b9ebd8396996e231d31b5b9dc7d
iaroslavski <43264149+iaroslavski@users.noreply.github.com>
2022-11-02 22:35:03 +0300

    Add files via upload

--- micro/r.bat
+++ micro/r.bat
@@ -1,4 +1,15 @@
 @echo off
 cls
 
-C:\Jdk\Jdk_x64\bin\java -jar target/benchmarks.jar org.openjdk.bench.java.util.Main
+rem Script run_jmh_sort.sh from Laurent
+rem
+rem #!/bin/bash
+rem export JAVA_OPTIONS="-Xms1g -Xmx1g"
+rem make test TEST="micro:java.util.ArraysSort" MICRO="FORK=1;RESULTS_FORMAT=json"
+
+call mvn clean package
+
+echo.
+echo Benchmarking...
+
+C:\Jdk\Jdk\bin\java -jar target/benchmarks.jar org.openjdk.bench.java.util.Main > 1.txt
